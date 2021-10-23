b6c656d40ee4c9aea625f6188e90e0f53607230c
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-10-23 17:32:37 +0200

    Added commits infos and display on hook

--- githublatestcommits.php
+++ githublatestcommits.php
@@ -18,6 +18,7 @@ class githublatestcommits extends Module implements WidgetInterface
         $this->name = 'githublatestcommits';
         $this->author = 'Thomas Huynh';
         $this->version = '1.0.0';
+        $this->tab = 'front_office_features';
         $this->ps_versions_compliancy = ['min' => '1.7.7.0', 'max' => _PS_VERSION_];
         $this->bootstrap = true;
 
@@ -36,15 +37,11 @@ class githublatestcommits extends Module implements WidgetInterface
      */
     public function install()
     {
-        if (!parent::install()) {
-            return false;
-        }
-
         if (parent::install() &&
             $this->registerHook('displayHome')
         ) {
-            Configuration::set('GIT_USER', 'KnpLabs');
-            Configuration::set('GIT_REPO', 'php-github-api');
+            Configuration::set('GIT_USER', 'KnpLabs', null);
+            Configuration::set('GIT_REPO', 'php-github-api', null);
             return true;
         }
 
@@ -79,61 +76,62 @@ class githublatestcommits extends Module implements WidgetInterface
 
     public function renderForm()
     {
-        $fields_options = array(
-            'Form' => array(
-                'title' => $this->trans('Github lastest commits configuration', [], 'Modules.githublatestcommits.Admin'),
-                'icon' => 'icon-cogs',
-                'fields' => array(
-                    'GITHUB_LATEST_COMMITS_USER' => array(
-                        'title' => $this->trans('Github user', [], 'Modules.githublatestcommits.Admin'),
+        $fields_form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->trans('Github lastest commits configuration', [], 'Modules.githublatestcommits.Admin'),
+                    'icon' => 'icon-cogs',
+                ],
+                'input' => [
+                    [
                         'type' => 'text',
-                    ),
-                    'GITHUB_LATEST_COMMITS_REPO' => array(
-                        'title' => $this->trans('Github repo', [], 'Modules.githublatestcommits.Admin'),
+                        'label' => $this->trans('Github user', [], 'Modules.githublatestcommits.Admin'),
+                        'name' => 'GITHUB_LATEST_COMMITS_USER',
+                    ],
+                    [
                         'type' => 'text',
-                    ),
-                ),
-                'submit' => array('title' => $this->trans('Save', [], 'Modules.githublatestcommits.Admin'))
-            ),
-        );
+                        'label' => $this->trans('Github repo', [], 'Modules.githublatestcommits.Admin'),
+                        'name' => 'GITHUB_LATEST_COMMITS_REPO',
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->getTranslator()->trans('Save', [], 'Admin.Actions'),
+                ],
+            ],
+        ];
 
         $helper = new HelperForm();
         $helper->show_toolbar = false;
         $helper->table = $this->table;
-        $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
-        $helper->default_form_language = $lang->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-
-        $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitGithubLatestCommits';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab
-            . '&module_name=' . $this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
         $helper->tpl_vars = [
             'fields_value' => $this->getConfigFieldsValues(),
         ];
 
-        return $helper->generateForm([$fields_options]);
+        return $helper->generateForm([$fields_form]);
     }
 
     /**
      *
      */
-    public function getContent() {
+    public function getContent()
+    {
         $output = '';
-        if (Tools::isSubmit('submitBlockCategories')) {
+        if (Tools::isSubmit('submitGithubLatestCommits')) {
             $user = Tools::getValue('GITHUB_LATEST_COMMITS_USER');
             $repo = Tools::getValue('GITHUB_LATEST_COMMITS_REPO');
 
-            if (empty($user)||empty($repo)) {
-                $output .= $this->displayError($this->getTranslator()->trans('Github User or Repo field are empty.', [], 'Admin.Notifications.Error'));
-            } else {
-                Configuration::updateValue('GIT_USER', Tools::getValue('GITHUB_LATEST_COMMITS_USER'));
-                Configuration::updateValue('GIT_REPO', Tools::getValue('GITHUB_LATEST_COMMITS_REPO'));
+//            if (empty($user)||empty($repo)) {
+//                $output .= $this->displayError($this->getTranslator()->trans('Github User or Repo field are empty.', [], 'Admin.Notifications.Error'));
+//            } else {
+            Configuration::updateValue('GIT_USER', Tools::getValue('GITHUB_LATEST_COMMITS_USER'));
+            Configuration::updateValue('GIT_REPO', Tools::getValue('GITHUB_LATEST_COMMITS_REPO'));
 
-                Tools::redirectAdmin(AdminController::$currentIndex . '&configure=' . $this->name . '&token=' . Tools::getAdminTokenLite('AdminModules') . '&conf=6');
-            }
+            Tools::redirectAdmin(AdminController::$currentIndex . '&configure=' . $this->name . '&token=' . Tools::getAdminTokenLite('AdminModules') . '&conf=6');
         }
+//        }
 
         return $output . $this->renderForm();
     }
@@ -163,11 +161,11 @@ class githublatestcommits extends Module implements WidgetInterface
     }
 
     /**
-     * Get commits from configuration
      * @param $hookName
      * @param array $configuration
+     * @return array
      */
-    public function getWidgetVariables($hookName, array $configuration)
+    public function getWidgetVariables($hookName, array $configuration): array
     {
         $gitUser = $configuration['git_user'] ?? null;
         $gitRepo = $configuration['git_repo'] ?? null;
@@ -180,16 +178,57 @@ class githublatestcommits extends Module implements WidgetInterface
             $repo = Configuration::get('GIT_REPO');
         }
 
+        return [
+            'commits' => $this->getCommits($user, $repo, 5),
+            'user' => $user,
+            'repo' => $repo
+        ];
+    }
+
+    public function hookDisplayHome()
+    {
+        $this->context->controller->registerStylesheet(
+            'githublatestcommits-style',
+            'modules/'.$this->name.'/views/css/githublatestcommits.css',
+            [
+                'media' => 'all',
+                'priority' => 200,
+            ]
+        );
+
+        $this->context->controller->registerStylesheet(
+            'bootstrap',
+            'modules/'.$this->name.'/views/css/bootstrap.min.css',
+            [
+                'media' => 'all',
+                'priority' => 200,
+            ]
+        );
+
+        return $this->renderWidget();
+    }
+
+    /**
+     * Get commits from configuration
+     * @param $user
+     * @param $repo
+     * @return array
+     */
+    public function getCommits($user, $repo, $numberOfCommits): array
+    {
         $client = new \Github\Client();
-        // client > endpoints > method/func (commits here)
+
         if (empty($user) && empty($repo)) {
             $commits = [];
         } else {
-            $commits = $client->api('repo')->commits()->all($user, $repo, array('sha' => 'master'));
+            try {
+                // client > endpoints > method/func (commits here)
+                $commits = $client->api('repo')->commits()->all($user, $repo, array('sha' => 'master'));
+            } catch (Exception $e) {
+                $commits = ['error' => 'error'];
+            }
         }
 
-        return [
-            'commits' => $commits
-        ];
+        return array_slice($commits, 0, $numberOfCommits, true);
     }
 }
--- views/templates/hook/githublatestcommits.tpl
+++ views/templates/hook/githublatestcommits.tpl
@@ -1,10 +1,25 @@
 {if $commits}
-    <div id="githublatestcommits">
-        {$commits|@var_dump}
-{*        <ul>*}
-{*            {foreach from=$commits item=commit}*}
-{*                <li>{$commit}</li>*}
-{*            {/foreach}*}
-{*        </ul>*}
-    </div>
+    {if 'error'|array_key_exists:$commits}
+        <div id="error">
+            <p>No commits are found, maybe the user or the repo doesn't exist or is not public</p>
+        </div>
+    {else}
+        <div class="container">
+            <div id="githublatestcommits">
+                <h3>Latest commits from {$user}'s repo ({$repo})</h3>
+                {foreach from=$commits item=element}
+                    <div class="commit row">
+                        <div class="align-self-center info col-6">
+                            <span><b>{$element.commit.author.name}</b></span>
+                            <span>{$element.commit.author.email}</span>
+                            <span>{$element.commit.author.date|date_format}</span>
+                        </div>
+                        <div class="message col-6">
+                            <p>{$element.commit.message}</p>
+                        </div>
+                    </div>
+                {/foreach}
+            </div>
+        </div>
+    {/if}
 {/if}
